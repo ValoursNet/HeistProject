5aa7ed9d414c23933cfdd1313e6e47823695bcb7
Michael Long <michael.long35@gmail.com>
2012-12-01 00:53:36 +0000

    + Sightline from player, with tile collisions

--- src/javagame/Play.java
+++ src/javagame/Play.java
@@ -1,5 +1,7 @@
 package javagame;
 
+import java.awt.Point;
+
 import org.newdawn.slick.*;
 import org.newdawn.slick.state.*;
 
@@ -26,7 +28,6 @@ public class Play extends BasicGameState{
 	@Override
 	public void init(GameContainer arg0, StateBasedGame arg1) throws SlickException {
 		player = new Image("res/PlayerAKDrawn.png");		
-		wall = new Image("res/wall.png");	
 	}
 
 	@Override
@@ -37,18 +38,72 @@ public class Play extends BasicGameState{
 		g.setColor(Color.red);
 		g.fillRect((float)playerX-19, (float)playerY-22, (float)40, (float)40);
 		
-		g.drawImage(wall,0,0);
 		g.drawImage(player,(int) playerX-39,(int) playerY-72);
 		
 		player.setCenterOfRotation(39,72);
 		player.setRotation(playerR);
+		
+		drawSightline(g);
+		drawSightlineCollision(g,levelOne.getMap());
 	}
 
 	@Override
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)throws SlickException {
 		inputHandler(gc);
 	}
+	
+	
+	private void drawSightline(Graphics g){
+		int lineLength = 500;
+		double playerRad = (playerR-90) * Math.PI / 180;
+		float startX = (float) playerX;
+		float startY = (float) playerY;
+		float endX   = (float) (startX + lineLength * Math.cos(playerRad));
+		float endY   = (float) (startY + lineLength * Math.sin(playerRad));
+		
+		g.setColor(Color.blue);
+		g.drawLine(startX, startY, endX, endY);
+	}
+	
+	private void drawSightlineCollision(Graphics g,int[][] mapArray){
+		int lineLength = 500;
+		double playerRad = (playerR-90) * Math.PI / 180;
+		float startX = (float) playerX;
+		float startY = (float) playerY;
+		
+		float endX   = (float) (startX+ 1 * Math.cos(playerRad));
+		float endY   = (float) (startY+ 1 * Math.sin(playerRad));
+		for (int i =0; i < lineLength; i++) {
+			endX   = (float) (startX + i * Math.cos(playerRad));
+			endY   = (float) (startY + i * Math.sin(playerRad));
+			
+			Point iP = getTileAtPoint(levelOne.tileSize,endX,endY);
+			
+			//Check if out of bounds
+			if(mapArray.length <= iP.y || iP.y < 0){
+				break;
+			}
+			if(mapArray[0].length <= iP.x || iP.x < 0){
+				break;
+			}
+			//Check for collision with solid tile
+			if(mapArray[iP.y][iP.x] == 1){
+				break;
+			}
+		}
+		
+		g.setColor(Color.red);
+		g.drawLine(startX, startY, endX, endY);
+	}
+	
+	private Point getTileAtPoint(int tileSize, double gX, double gY){
+		int rX = (int) (gX/tileSize);
+		int rY = (int) (gY/tileSize);;
 
+		Point rP = new Point(rX,rY);
+		return rP;
+	}
+	
 	private void drawMap(int[][] mapArray,Graphics g){
 		int tSize = levelOne.tileSize;
 		for (int i =0; i < mapArray.length; i++) {
